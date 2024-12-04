240fd3bc654f5e3f1418816d7a94bb25a8d3b085
Rajib <saidul.is.rajib@gmail.com>
2024-12-04 16:52:21 +0600

    Add post response

---
+++ Sober/Sober.Contracts/Response/Posts/PostResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace Sober.Contracts.Response.Posts
+{
+    public record PostResponse(
+        Guid postId,
+        string postTitle,
+        string postAbstract,
+        List<PostSectionResponse> Sections,
+        List<TopicResponse> Topics,
+        string userId,
+        DateTime CreatedDateTime,
+        DateTime UpdatedDateTime);
+
+    public record PostSectionResponse(
+        string sectionId,
+        string sectionTitle,
+        string sectionDescription,
+        List<PostSectionItemResponse> Items);
+
+    public record PostSectionItemResponse(
+        string itemId,
+        string itemTitle,
+        string itemDescription,
+        string itemImageLink);
+
+    public record TopicResponse(
+        string topicId,
+        string topicTitle,
+        string userId);
+}
