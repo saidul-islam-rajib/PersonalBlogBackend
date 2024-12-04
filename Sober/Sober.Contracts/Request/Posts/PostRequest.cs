a7228fea0c6988365627c024445b3344a263a62e
Rajib <saidul.is.rajib@gmail.com>
2024-12-04 16:45:12 +0600

    Add post request model

---
+++ Sober/Sober.Contracts/Request/Posts/PostRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace Sober.Contracts.Request.Posts
+{
+    public record PostRequest(
+        string postTitle,
+        string postAbstract,
+        List<PostSectionRequest> Sections,
+        List<TopicRequest> Topics);
+
+    public record PostSectionRequest(
+        string sectionTitle,
+        string sectionDescription,
+        List<PostSectionItemRequest> Items);
+
+    public record PostSectionItemRequest(
+        string itemTitle,
+        string itemDescription,
+        string itemImageLink);
+
+    public record TopicRequest(
+        string topicTitle,
+        string userId);
+
+}
