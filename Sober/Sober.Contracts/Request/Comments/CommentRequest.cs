36d8e28d9d11515719800bd0b2b1c3f7d9f19a5e
Rajib <saidul.is.rajib@gmail.com>
2024-12-09 15:34:02 +0600

    Add `comment` request/response model

---
+++ Sober/Sober.Contracts/Request/Comments/CommentRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Sober.Contracts.Request.Comments
+{
+    public record CommentRequest(
+        string PostId,
+        string GuestName,
+        string Comment);
+}
---
+++ Sober/Sober.Contracts/Response/Comments/CommentResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Sober.Contracts.Response.Comments
+{
+    public record CommentResponse(
+        Guid CommentId,
+        string PostId,
+        string PostTitle,
+        string CommentorName,
+        string Comment,
+        DateTime date);
+}
