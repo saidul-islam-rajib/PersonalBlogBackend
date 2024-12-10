f78b03aeb8ae7003c337408ea7ddd943fe379d9d
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-10 10:33:08 +0600

    Add education request/response

---
+++ Sober/Sober.Contracts/Request/EducationRequest.cs
@@ -0,0 +1,12 @@
+﻿using Sober.Contracts.Request.Skills;
+
+namespace Sober.Contracts.Request
+{
+    public record EducationRequest(
+        string InstituteName,
+        string InstituteLogo,
+        string Department,
+        DateTime StartDate,
+        DateTime EndDate,
+        List<SkillRequest> Skills);
+}
---
+++ Sober/Sober.Contracts/Response/EducationResponse.cs
@@ -0,0 +1,13 @@
+﻿using Sober.Contracts.Response.Skills;
+
+namespace Sober.Contracts.Response
+{
+    public record EducationResponse(
+        Guid EducationId,
+        string InstituteName,
+        string InstituteLogo,
+        string Department,
+        DateTime StartDate,
+        DateTime EndDate,
+        List<SkillResponse> Skills);
+}
