a6eb0519f485c8e28a5b376bfa0d4305714df79b
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-10 17:29:15 +0600

    Add experience interface

---
+++ Sober/Sober.Application/Interfaces/IExperienceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sober.Domain.Aggregates.ExperienceAggregate;
+
+namespace Sober.Application.Interfaces
+{
+    public interface IExperienceRepository
+    {
+        void AddExperience(Experience experience);
+        bool DeleteExperience(Guid commentId);
+        Task<IEnumerable<Experience>> GetAllExperienceAsync();
+        Task<Experience> GetExperienceByIdAsync(Guid experienceId);
+    }
+}
