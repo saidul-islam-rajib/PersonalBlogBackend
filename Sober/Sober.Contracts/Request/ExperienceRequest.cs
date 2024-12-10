a479ee95a0e2cd64bfd2136ec85f53029194914d
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-10 10:38:17 +0600

    Add experience request/response model

---
+++ Sober/Sober.Contracts/Request/ExperienceRequest.cs
@@ -0,0 +1,14 @@
+﻿using Sober.Contracts.Request.Skills;
+
+namespace Sober.Contracts.Request
+{
+    public record ExperienceRequest(
+        string companyName,
+        string companyLogo,
+        string designation,
+        bool isCurrentEmployee,
+        DateTime startDate,
+        DateTime endDate,
+        bool isFullTimeEmployee,
+        List<SkillRequest> skill);
+}
---
+++ Sober/Sober.Contracts/Response/ExperienceResponse.cs
@@ -0,0 +1,17 @@
+﻿using Sober.Contracts.Response.Skills;
+
+namespace Sober.Contracts.Response
+{
+    public record ExperienceResponse(
+        Guid experienceId,
+        string companyName,
+        string companyLogo,
+        string designation,
+        bool isCurrentEmployee,
+        DateTime startDate,
+        DateTime endDate,
+        bool isFullTimeEmployee,
+        List<SkillResponse> skill,
+        DateTime createdDate,
+        DateTime updatedDate);
+}
