c83343e2c456ba7e2fb83fd51253ed8efb6cb2a6
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-10 11:32:35 +0600

    Add skill query/query handler

---
+++ Sober/Sober.Application/Pages/Skills/Queries/Query/GetAllSkillQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Sober.Domain.Aggregates.SkillAggregate;
+
+namespace Sober.Application.Pages.Skills.Queries.Query
+{
+    public record GetAllSkillQuery : IRequest<IEnumerable<Skill>>
+    {
+    }
+}
---
+++ Sober/Sober.Application/Pages/Skills/Queries/QueryHandler/GetAllSkillQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Sober.Application.Interfaces;
+using Sober.Application.Pages.Skills.Queries.Query;
+using Sober.Domain.Aggregates.SkillAggregate;
+
+namespace Sober.Application.Pages.Skills.Queries.QueryHandler
+{
+    public class GetAllSkillQueryHandler : IRequestHandler<GetAllSkillQuery, IEnumerable<Skill>>
+    {
+        private readonly ISkillRepository _skillRepository;
+
+        public GetAllSkillQueryHandler(ISkillRepository skillRepository)
+        {
+            _skillRepository = skillRepository;
+        }
+
+        public async Task<IEnumerable<Skill>> Handle(GetAllSkillQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _skillRepository.GetAllSkillAsync();
+            return response;
+        }
+    }
+}
