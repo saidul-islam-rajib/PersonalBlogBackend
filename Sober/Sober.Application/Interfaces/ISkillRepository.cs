6b490f55ce1744b9b7e75fe6b0c1852fc915dfce
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-10 11:33:06 +0600

    Add skill interface

---
+++ Sober/Sober.Application/Interfaces/ISkillRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sober.Domain.Aggregates.SkillAggregate;
+
+namespace Sober.Application.Interfaces
+{
+    public interface ISkillRepository
+    {
+        void CreateSkill(Skill skill);
+        bool DeleteSkill(Guid skillId);
+        Task<IEnumerable<Skill>> GetAllSkillAsync();
+        Task<Skill> GetSkillByIdAsync(Guid skillId);
+    }
+}
