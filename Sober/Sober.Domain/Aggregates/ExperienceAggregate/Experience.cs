d569543099a965d21a3bc40458ecf75b6e619f86
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-10 11:33:38 +0600

    Add experience aggregates

---
+++ Sober/Sober.Domain/Aggregates/ExperienceAggregate/Experience.cs
@@ -0,0 +1,9 @@
+﻿using Sober.Domain.Aggregates.ExperienceAggregate.ValueObjects;
+using Sober.Domain.Common.Models;
+
+namespace Sober.Domain.Aggregates.ExperienceAggregate
+{
+    public class Experience : AggregateRoot<ExperienceId>
+    {
+    }
+}
