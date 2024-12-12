3a48dac775284e3e9dda2f99c8eb2c319b7f4743
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-12 14:07:33 +0600

    Modified migration and added 2 new table and updated max size of some columns

---
+++ Sober/Sober.Infrastructure/Migrations/20241212080433_Modified Posts tables and added 2 columns.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sober.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedPoststablesandadded2columns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SectionDescription",
+                table: "Sections",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostAbstract",
+                table: "Posts",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Conclusion",
+                table: "Posts",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReadingMinute",
+                table: "Posts",
+                type: "int",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostItemDescription",
+                table: "PostItems",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Conclusion",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ReadingMinute",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SectionDescription",
+                table: "Sections",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostAbstract",
+                table: "Posts",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostItemDescription",
+                table: "PostItems",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
